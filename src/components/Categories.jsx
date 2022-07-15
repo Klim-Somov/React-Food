a18ade5177e498f1c182e64bcb2ebcf02c1c410f
Klim <klim_somov@mail.ru>
2022-07-15 13:25:18 +0300

    make live categories chose

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const categoriesList = [
+  "Все",
+  "Мясные",
+  "Вегетарианская",
+  "Гриль",
+  "Острые",
+  "Закрытые",
+];
 
 function Categories() {
+  const [activeCategory, setActiveCategory] = useState(0);
+
   return (
     <div className="categories">
-    <ul>
-      <li className="active">Все</li>
-      <li>Мясные</li>
-      <li>Вегетарианская</li>
-      <li>Гриль</li>
-      <li>Острые</li>
-      <li>Закрытые</li>
-    </ul>
-  </div>
-  )
+      <ul>
+        {categoriesList.map((cat, i) => {
+          return (
+            <li
+              onClick={() => setActiveCategory(i)}
+              key={i}
+              className={activeCategory === i ? "active" : ""}
+            >
+              {cat}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default Categories
+export default Categories;
