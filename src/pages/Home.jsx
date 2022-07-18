2e6950287146df60f8196584bd961f007dfed5af
Klim <klim_somov@mail.ru>
2022-07-18 21:20:34 +0300

    implement searching by imput on backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,18 +4,22 @@ import Loader from "../components/Loader";
 import PizzaBlock from "../components/PizzaBlock";
 import Sort from "../components/Sort";
 
-function Home() {
+function Home({ searchValue }) {
+
   const [items, setitems] = useState([]);
   const [loading, setoading] = useState(false);
+  const pizzas = items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />);
+  const search = searchValue ? `search=${searchValue}` : "";
+
   useEffect(() => {
-    fetch("https://62d15c8ddccad0cf1765fbd3.mockapi.io/items")
+    fetch(`https://62d15c8ddccad0cf1765fbd3.mockapi.io/items?${search}`)
       .then((response) => response.json())
       .then((arr) => {
         setitems(arr);
         setoading(true);
       });
-      window.scrollTo(0, 0);
-  }, []);
+    window.scrollTo(0, 0);
+  }, [searchValue]);
   return (
     <>
       <div className="content__top">
@@ -25,7 +29,7 @@ function Home() {
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {loading
-          ? items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)
+          ? pizzas
           : [...new Array(6)].map((_, index) => <Loader key={index} />)}
       </div>
     </>
