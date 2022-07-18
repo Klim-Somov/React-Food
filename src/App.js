979409d74527ecd771e74171df13c79726009656
Klim <klim_somov@mail.ru>
2022-07-18 14:05:12 +0300

    adaptive

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Categories from "./components/Categories";
 import Header from "./components/Header";
+import Loader from "./components/Loader";
 import PizzaBlock from "./components/PizzaBlock";
 import Sort from "./components/Sort";
-import pizzas from "./assets/pizzas.json";
 
 import "./scss/app.scss";
 
 function App() {
   const [items, setitems] = useState([]);
+  const [loading, setoading] = useState(false);
 
   useEffect(() => {
     fetch("https://62d15c8ddccad0cf1765fbd3.mockapi.io/items")
       .then((response) => response.json())
       .then((arr) => {
         setitems(arr);
+        setoading(true);
       });
   }, []);
 
@@ -29,9 +31,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((pizza) => (
-              <PizzaBlock key={pizza.id} {...pizza} />
-            ))}
+            {loading
+              ? items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)
+              : [...new Array(6)].map((_, index) => <Loader key={index} />)}
           </div>
         </div>
       </div>
---
+++ src/components/Loader.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const Loader = (props) => (
+  <ContentLoader 
+    speed={2}
+    width={300}
+    height={480}
+    viewBox="0 0 245 480"
+    backgroundColor="#c2c2c2"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <circle cx="120" cy="112" r="103" /> 
+    <rect x="0" y="272" rx="8" ry="8" width="245" height="88" /> 
+    <rect x="86" y="273" rx="0" ry="0" width="17" height="0" /> 
+    <rect x="0" y="222" rx="13" ry="13" width="245" height="27" />
+  </ContentLoader>
+)
+
+export default Loader
