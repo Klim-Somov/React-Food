dd26d156355e74fbe78e536273cd03139801dcca
Klim <klim_somov@mail.ru>
2022-08-18 22:02:21 +0300

    implement axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Categories from "../components/Categories";
@@ -6,7 +7,6 @@ import PizzaBlock from "../components/PizzaBlock";
 import Sort from "../components/Sort";
 import Pagination from "../pagination/Pagination";
 function Home({ searchValue }) {
-
   const [items, setitems] = useState([]);
   const [loading, setLoading] = useState(false);
   const sortObject = useSelector((store) => store.filter.sort);
@@ -15,19 +15,25 @@ function Home({ searchValue }) {
   const serarchBycategory = categoryId > 0 ? `category=${categoryId}` : "";
   const pizzas = items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />);
   const search = searchValue ? `search=${searchValue}` : "";
+
   useEffect(() => {
     setLoading(false);
-    fetch(
-      `https://62d15c8ddccad0cf1765fbd3.mockapi.io/items?${serarchBycategory}&sortBy=${sortObject.sortProperty}&order=desc${search}`
-    )
-      .then((response) => response.json())
-      .then((arr) => {
-        setitems(arr);
-        setLoading(true);
-        console.log(arr);
-      });
-    window.scrollTo(0, 0);
+    fetchPizza();
   }, [searchValue, categoryId, sortObject]);
+
+  const fetchPizza = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://62d15c8ddccad0cf1765fbd3.mockapi.io/items?${serarchBycategory}&sortBy=${sortObject.sortProperty}&order=desc${search}`
+      );
+      setitems(data);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(true);
+      window.scrollTo(0, 0);
+    }
+  };
 
   return (
     <>
