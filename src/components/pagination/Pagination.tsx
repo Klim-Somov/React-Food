554ab07d1b6683c16093313cf8a60aeb44f23713
Klim <klim_somov@mail.ru>
2022-09-03 12:09:21 +0300

    типизация prop функции в пагинации

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import ReactPaginate from "react-paginate";
 import styles from "./pagination.module.scss";
 
-
-
-const Pagination: React.FC= ({onChangePage}) => {
+type PaginationProps = {
+  onChangePage: (i: number) => void;
+};
+const Pagination: React.FC<PaginationProps> = ({ onChangePage }) => {
   return (
     <>
       <ReactPaginate
         className={styles.root}
         breakLabel="..."
         nextLabel=" >"
-        onPageChange={(e) => onChangePage(e.selected +1)}
+        onPageChange={(e) => onChangePage(e.selected + 1)}
         pageRangeDisplayed={6}
         pageCount={3}
         previousLabel="< "
